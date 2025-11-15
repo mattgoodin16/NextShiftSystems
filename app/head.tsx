a582cc4da2e096bea089b82116459f0892208303
mattgoodin16 <mattgoodin31@gmail.com>
2025-11-15 10:05:15 -0800

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="<meta name="google-site-verification" content="0_gtx03YnncjXE6dkCDcHgYoJ4YO8hnTdknPnrVSxM8" />"
+      />
+    </>
+  );
+}
